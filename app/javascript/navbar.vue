bbf342e329cdfd38c2fdf95d9ee0cae34e2883bf
Ryuichiroh Ikeuchi <ryicoh@gmail.com>
2018-12-19 08:04:39 +0900

    generate page controller

---
+++ app/javascript/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="navbar" class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="navbar-brand">
+      レビコミ
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
